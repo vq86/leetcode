d2a541a47c3baa1651f64a19d98e0549c03ce7d5
vq86 <vicky0105.wq@gmail.com>
2015-09-11 15:22:22 +0800

    add problem description

--- rotatearray.py
+++ rotatearray.py
@@ -1,3 +1,8 @@
+# Problem description:
+# Rotate an array of n elements to the right by k steps.
+# For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+
 def rotate(num, k):
     length = len(num)
     step = k % length
